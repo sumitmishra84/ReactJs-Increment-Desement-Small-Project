94b90f4d3a2a85b237e124b4c5eacf1b2a230ee0
Sumit Mishra <sm0141136@gmail.com>
2024-08-27 15:26:09 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -2,22 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  function decreaseHandler(){
+  
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='w-[100vw] h-[100vh] flex justify-center items-center bg-[#344151] flex-col gap-10'>
+      <div className='text-[#0398d4] fonst-medium text-size-2xl'>Increment && Decrement</div>
+      <div className='bg-white flex justify-center gap-12 py-3 rounded-sm text-[25px] text-[#344151]'>
+        <button onClick={(decreaseHandler)}>
+          -
+        </button>
+        <div>
+
+        </div>
+        <button>
+          +
+        </button>
+      </div>
+      <button>
+        reset
+      </button>
     </div>
   );
 }
